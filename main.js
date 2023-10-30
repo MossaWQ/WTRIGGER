5e4ac31df86fb62f959bf087413f4644c0a33c67
MossaWQ <149276729+MossaWQ@users.noreply.github.com>
2023-10-30 15:21:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+const __Words = ["PerformHttpRequest", "GetConvar", "print", "execute", "command", "txAdmin"]
+const __foundScripts = []
+
+function printColor(text, color) {
+  const __Colors = {
+        red: "^1",
+        green: "^2",
+        yellow: "^3",
+        blue: "^4",
+        lightblue: "^5",
+        purple: "^6",
+        white: "^7",
+        black: "^8"
+  }
+
+
+  const __Code = __Colors[color] + text;
+  console.log(__Code)
+}
+
+
 
