a40a82df922c90f638614b75a3708d01ea62403f
MossaWQ <149276729+MossaWQ@users.noreply.github.com>
2023-10-30 15:24:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-const __Words = ["PerformHttpRequest", "GetConvar", "print", "execute", "command", "txAdmin"]
+const __Words = ["PerformHttpRequest", "GetConvar", "print", "execute", "command", "txAdmin", "xpcall"]
 const __foundScripts = []
+
+
+const __Settings = {
+
+  Enabeld: true,
+  ConsoleDetection: true,
+  StopServer: false,
+  Backdoor_Strings: [
+    "cipher-panel",
+    "Enchanced_Tabs",
+    "helperServer",
+    "ketamin.cc",
+    "\x63\x69\x70\x68\x65\x72\x2d\x70\x61\x6e\x65\x6c\x2e\x6d\x65",
+    "\x6b\x65\x74\x61\x6d\x69\x6e\x2e\x63\x63",
+    "MpWxwQeLMRJaDFLKmxVIFNeVfzVKaTBiVRvjBoePYciqfpJzxjNPIXedbOtvIbpDxqdoJR"
+  ]
+
+}
+
+
 
 function printColor(text, color) {
   const __Colors = {
